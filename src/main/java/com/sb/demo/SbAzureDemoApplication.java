1afe58f33c84386d3f634d247cb9d94877845d2c
ilamvku25-tech <ilamvku25@gmail.com>
2025-08-10 08:51:17 -0400

    first commit

---
+++ src/main/java/com/sb/demo/SbAzureDemoApplication.java
@@ -0,0 +1,22 @@
+package com.sb.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SbAzureDemoApplication {
+
+	@GetMapping("/message")
+	public String message(){
+		return "congrats ! your app successfully deployed in Azure";
+	}git
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SbAzureDemoApplication.class, args);
+	}
+
+}
